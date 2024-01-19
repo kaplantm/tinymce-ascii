75c230ca5c4d84f65b7d8a1351d04ac631d4b169
kaplantm <tonarie.k@gmail.com>
2024-01-19 00:57:31 -0500

    temp

--- src/components/TinyEditorView.tsx
+++ src/components/TinyEditorView.tsx
@@ -1,23 +1,16 @@
-import React, { useRef } from "react";
 import { Editor } from "@tinymce/tinymce-react";
-import { RawEditorOptions } from "tinymce";
+import { ComponentProps } from "react";
 
 type TinyEditorViewProps = {
   copy: string;
-} & RawEditorOptions;
+} & ComponentProps<typeof Editor>;
 
-export default function TinyEditorView({ copy, ...rawEditorOptions }: TinyEditorViewProps) {
-  const editorRef = useRef(null);
-  // const log = () => {
-  //   if (editorRef.current) {
-  //     console.log(editorRef.current.getContent());
-  //   }
-  // };
+const TinyEditorView = ({ copy, onEditorChange, init }: TinyEditorViewProps) => {
   return (
     <>
       <Editor
+        onEditorChange={onEditorChange}
         apiKey={import.meta.env.VITE_TINY_MCE_API_KEY}
-        onInit={(evt, editor) => (editorRef.current = editor)}
         initialValue={copy}
         init={{
           height: window.innerHeight * 0.8,
@@ -48,12 +41,14 @@ export default function TinyEditorView({ copy, ...rawEditorOptions }: TinyEditor
             "bold italic forecolor | alignleft aligncenter " +
             "alignright alignjustify | bullist numlist outdent indent | " +
             "removeformat | help",
-          content_style:
-            "body { font-family:Courier New,monospace,sans-serif; font-size:8px; line-height: 4px; letter-spacing:-1px; text-align:center; font-weight:800; white-space:nowrap }",
-          ...rawEditorOptions,
+          // content_style:
+          //   "body { font-family:Courier New,monospace,sans-serif; font-size:8px; line-height: 4px; letter-spacing:-1px; text-align:center; font-weight:800; white-space:nowrap }",
+          ...init,
         }}
       />
       {/* <button onClick={log}>test</button> */}
     </>
   );
-}
+};
+
+export default TinyEditorView;
